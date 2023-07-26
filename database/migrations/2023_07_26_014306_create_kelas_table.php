6aba703e698547ad503f7b151e0738ffd07a9914
ajibkholiq <ajib.kholiq@gmail.com>
2023-07-26 09:21:05 +0700

    create migration tahun pelajaran, semester, kelas, dan siswa

---
+++ database/migrations/2023_07_26_014306_create_kelas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid',20);
+            $table->string('kelas',20);
+            $table->enum('tingkat', ['1','2','3','4','5','6','7','8','9','10','11','12']);
+            $table->string('remark',20);
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kelas');
+    }
+};
